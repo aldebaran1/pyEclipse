37f7011d23814ed021c9fbe6a056faa431b7f64f
aldebaran1 <sebastijan.mrak@gmail.com>
2021-04-29 14:26:20 -0400

    No duplicate files within a directory

--- sdomask_lonlat_cli.py
+++ sdomask_lonlat_cli.py
@@ -19,7 +19,7 @@ def _eof(sep, azm, eta, mrad, wl):
     return utils.get_eof_mask_from_angles(SDO['AIA{}'.format(wl)].values, sep,azm,eta,mrad, x0, y0, imres=imres,pixscale=rad2pix)
 
 def main(startend, odir, tsdo=None, glonlim=[-180,180], glatlim=[-90,90], alt_km=0, 
-         wl=193, dlon=None, dlat=None, dt=10, 
+         wl=193, dlon=None, dlat=None, dt=10, j=1,
          aiafolder=None,
          ):
     global SDO, x0, y0, imres, rad2pix
@@ -61,43 +61,40 @@ def main(startend, odir, tsdo=None, glonlim=[-180,180], glatlim=[-90,90], alt_km
     critical_distance = imres/2 + imres*1.414
     
     for it,T in enumerate(times):
-        print ("Processing {} // {}/{}".format(T, it+1, times.size))
-        tt = datetime.now()
-        of = np.nan * np.ones((glat.size, glon.size))
-        sza = utils.get_sza(T,glon=glon_grid,glat=glat_grid,alt_km=alt_km)
-        sep, azm, mrad = utils.get_angles(T, glon=glon_grid, glat=glat_grid, ghgt=ghgt)
-        eta = utils.get_parallactic_angle(T, glon=glon_grid, glat=glat_grid, ghgt=ghgt)
-        
-        mask = (sep*rad2pix > critical_distance)
-        of[mask] = 1.0
-        mask = (sza > 90)
-        of[mask] = 0.0
+        save_fn = odir + "{}A_{}km_{}.nc".format(wl, int(alt_km), T.strftime("%Y%m%d%H%M%S"))
+        if not os.path.exists(save_fn):
+            print ("Processing {} // {}/{}".format(T, it+1, times.size))
+            tt = datetime.now()
+            of = np.nan * np.ones((glat.size, glon.size))
+            sza = utils.get_sza(T,glon=glon_grid,glat=glat_grid,alt_km=alt_km)
+            sep, azm, mrad = utils.get_angles(T, glon=glon_grid, glat=glat_grid, ghgt=ghgt)
+            eta = utils.get_parallactic_angle(T, glon=glon_grid, glat=glat_grid, ghgt=ghgt)
+            
+            mask = (sep*rad2pix > critical_distance)
+            of[mask] = 1.0
+            mask = (sza > 90)
+            of[mask] = 0.0
 
-        ix = np.where(np.isnan(of))
-        print ("Loading for one iteration {}".format(datetime.now()-tt))
-        C = ix[0].size
-        print ("Eclipse possible in {} pixels".format(C))
-    #    for k in range(C):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=50) as ex:
-            of_worker = np.asarray([ex.submit(_eof, sep[ix[0][i],ix[1][i]], azm[ix[0][i],ix[1][i]], eta[ix[0][i],ix[1][i]], mrad[ix[0][i],ix[1][i]],wl) for i in range(ix[0].size)])
-        print ("Total {}".format(datetime.now()-tt))
-    #    of_temp = np.nan*np.ones((glon.size, glat.size))
-        for i in range(of_worker.size):
-            of[ix[0][i],ix[1][i]] = of_worker[i].result()
-    #        of = utils.mask_lonlat_sdo(SDO, T, glon, glat, ghgt, wl=wl, verbose=False)
-        # TO XARRAY
-        X = xarray.Dataset(
-            {
-                "of": (("glat", "glon"), of),
-            },
-            {"glon": glon, "glat": glat}
-        )
-        X['time'] = T
-        X['alt_km'] = alt_km
-        X['wl'] = wl
-        X['time_sdo'] = SDO.time
-        X.to_netcdf(odir + "{}A_{}km_{}.nc".format(wl, int(alt_km), T.strftime("%Y%m%d%H%M%S")))
-        X.close()
+            ix = np.where(np.isnan(of))
+            print ("Loading for one iteration {}".format(datetime.now()-tt))
+            with concurrent.futures.ThreadPoolExecutor(max_workers=j) as ex:
+                of_worker = np.asarray([ex.submit(_eof, sep[ix[0][i],ix[1][i]], azm[ix[0][i],ix[1][i]], eta[ix[0][i],ix[1][i]], mrad[ix[0][i],ix[1][i]],wl) for i in range(ix[0].size)])
+            print ("Total {}".format(datetime.now()-tt))
+            for i in range(of_worker.size):
+                of[ix[0][i],ix[1][i]] = of_worker[i].result()
+            # TO XARRAY
+            X = xarray.Dataset(
+                {
+                    "of": (("glat", "glon"), of),
+                },
+                {"glon": glon, "glat": glat}
+            )
+            X['time'] = T
+            X['alt_km'] = alt_km
+            X['wl'] = wl
+            X['time_sdo'] = SDO.time
+            X.to_netcdf(save_fn)
+            X.close()
 
 if __name__ == '__main__':
     p = ArgumentParser()
@@ -112,9 +109,10 @@ if __name__ == '__main__':
     p.add_argument('--sdodir', help='Folder to SDO images', default=None)
     p.add_argument('-w', '--wl', help='Choose the wavelength. Default is 193A for the SDO', default = 193, type=int)
     p.add_argument('--altkm', help='altitude in km', default = 350, type=int)
+    p.add_argument('-j', '--proc', help='Numer of parallel processes', default=10, type=int)
     
     P = p.parse_args()
     
     main(P.startend, odir=P.odir, dt = P.tres, tsdo = P.tsdo,
          glonlim=P.glon, glatlim=P.glat, dlon=P.dlon, dlat=P.dlat,
-         wl = P.wl, alt_km= P.altkm, aiafolder = P.sdodir)
+         wl = P.wl, alt_km= P.altkm, aiafolder = P.sdodir, j=P.proc)
