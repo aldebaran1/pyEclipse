2c0ff6a29678dfcb0ebac10eca0d2f99c82a4d4c
aldebaran1 <sebastijan.mrak@gmail.com>
2021-06-24 20:53:43 -0400

    geo srad factor

--- nc2png_cli.py
+++ nc2png_cli.py
@@ -5,7 +5,7 @@ Created on Mon Nov  9 19:41:58 2020
 @author: smrak@bu.edu
 """
 
-import xarray, os, glob
+import xarray, os, glob, datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import cartomap.geogmap as gm
@@ -19,7 +19,7 @@ projection = 'ortographic'
 mlat_levels = [-65, -75, 60, 75]
 
 def main(idir=None, wl=None, alt_km=None, odir=None, tlim=None,
-         laplace=0,auroral_oval=0,clabel=0,
+         laplace=0,auroral_oval=0,clabel=0, srad=1.0,
          lat0 = None, lon0 = None, save=1):
     cmax = 10 if clabel else 25
     clw = 1 if clabel else 0.5
@@ -30,15 +30,24 @@ def main(idir=None, wl=None, alt_km=None, odir=None, tlim=None,
         apx = 0
         
     if odir is None:
-        if laplace:
-            odir = os.path.join(idir, "{}_{}_lap\\".format(wl, alt_km))
+        if wl != 'geo':
+            if laplace:
+                odir = os.path.join(idir, "{}_{}_lap\\".format(wl, alt_km))
+            else:
+                odir = os.path.join(idir, "{}_{}\\".format(wl, alt_km))
         else:
-            odir = os.path.join(idir, "{}_{}\\".format(wl, alt_km))
+            if laplace:
+                odir = os.path.join(idir, "{}_{}_{}_lap\\".format(wl, srad, alt_km))
+            else:
+                odir = os.path.join(idir, "{}_{}_{}\\".format(wl, srad, alt_km))
     if not os.path.exists(odir):
         subprocess.call('mkdir "{}"'.format(odir), timeout=2, shell=True)
     assert os.path.exists(odir)
     
-    EOFF = np.array(sorted(glob.glob(idir + "*_{}km_{}*.nc".format(alt_km, wl))))
+    if wl != 'geo':
+        EOFF = np.array(sorted(glob.glob(idir + "*_{}km_{}*.nc".format(alt_km, wl))))
+    else:
+        EOFF = np.array(sorted(glob.glob(idir + "*_{}km_{}*_{}.nc".format(alt_km, wl, srad))))
     f_times = []
     for f in EOFF:
         try:
@@ -56,20 +65,27 @@ def main(idir=None, wl=None, alt_km=None, odir=None, tlim=None,
         idt = (f_times >= tlim[0]) & (f_times <= tlim[1])
     #
     F = EOFF[idt]
+    
+    
     #
     for i,f in enumerate(F):
         t = f_times[idt][i]
         save_fn = odir + "{}_{}_{}.png".format(t.strftime("%Y%m%d_%H%M%S"), alt_km, wl)
         EOF = xarray.open_dataset(f)
+        if wl != 'geo':
+            t_sdo = EOF.time_sdo.values.astype('datetime64[s]').astype(datetime.datetime).strftime("%Y-%m-%d %H:%M")
+            title = '{}, Alt = {} km\nTSDO = {}'.format(t, EOF.alt_km.values, t_sdo)
+        else:
+            title = '{}, Alt = {} km'.format(t, EOF.alt_km.values)
         fig, ax = gm.plotCartoMap(projection=projection, lon0=lon0, lat0=lat0,
                                   figsize=[8,5],
                                   lonlim=[-180,180], latlim=[-90,90],
-                                  title = '{}, Alt = {} km'.format(t, EOF.alt_km.values),
+                                  title = title,
                                   meridians=np.arange(-180,180.1,40), parallels=np.arange(-80,81,20),
                                   background_color='grey',
                                   apex=apx, mlat_levels=mlat_levels, mlat_colors='b',
                                   mlat_labels=0)
-        
+
         sza = EOF.sza.values
         night = np.copy(sza)
         inight = (sza > 90)
@@ -134,6 +150,7 @@ if __name__ == '__main__':
     p.add_argument('--odir', help='Save images to:', default=None)
     p.add_argument('--altkm', help='altitude in km', default = 150, type=int)
     p.add_argument('--tlim', help='time limit, 2 args "start" "stop"', nargs=2, default=None, type=str)
+    p.add_argument('--srad', help='altitude in km', default = 1.0, type=float)
     
     p.add_argument('--lon0', help='Map center: longitude default = 0', type=int, default=0)
     p.add_argument('--lat0', help='Map center: Latitude default = 0', type=int, default=0)
@@ -144,6 +161,6 @@ if __name__ == '__main__':
     
     P = p.parse_args()
     main(idir=P.folder, wl=P.wl, alt_km=P.altkm, odir=P.odir, tlim=P.tlim,
-         laplace=P.laplace, auroral_oval=P.oval, clabel=P.clabel,
+         laplace=P.laplace, auroral_oval=P.oval, clabel=P.clabel, srad=P.srad,
          lat0=P.lat0, lon0=P.lon0, save=1)
     
